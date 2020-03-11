bd72f2d742fa7c0b9fd189d7ef7ee7eaaf02e3c4
Igor <gogon65@mail.ru>
2020-03-11 23:14:40 +0300

    Pushing first build

---
+++ App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by Igor Lebedev on 13/02/2020.
+//  Copyright © 2020 Igor Lebedev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
